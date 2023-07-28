b7ac6388197af02efde15c3445906ad4a9d7eb7d
GitGitHubGiteeGitLab <130532493+GitGitHubGiteeGitLab@users.noreply.github.com>
2023-07-28 17:34:09 +0800

    常规找素数, I = sqrt(n),sqrt(n)只用算一次

---
+++ C_Programming/Algorithm/Sieve/22_2.c
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: 22_2.c
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 28 Jul 2023 05:26:21 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<math.h>
+#define N 10000000
+#include<stdio.h>
+#include<math.h>
+#define N 10000000
+
+int is_prime(int n){
+    for(int i = 2, I  = sqrt(n) + 1; i < I; i++){//sqrt(n)只用算一次
+        if(n % i)continue;
+        return 0;
+    }
+    return 1;
+}
+
+int main(){
+    for(int i = 1; i < N + 1; i++){
+        if(!is_prime(i))continue;
+        printf("%d\n", i);
+    }
+    return 0;
+}
