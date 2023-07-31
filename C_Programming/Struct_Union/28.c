366be4b55b8fbf382d4a32971aab9511c06a3226
GitGitHubGiteeGitLab <130532493+GitGitHubGiteeGitLab@users.noreply.github.com>
2023-07-31 08:32:32 +0800

    IP地址转整数9（小端序）

---
+++ C_Programming/Struct_Union/28.c
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: 28.c
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 31 Jul 2023 07:27:35 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+union IP{
+    struct {
+        unsigned char a1;
+        unsigned char a2;
+        unsigned char a3;
+        unsigned char a4;
+    }bytes;
+    unsigned int num;
+};
+
+int main(){
+    union IP p;
+    char str[30] = {0};
+    int arr[4];
+    while(~scanf("%s", str)){
+        getchar();//无必要
+        sscanf(str, "%d.%d.%d.%d", &arr[0], &arr[1], &arr[2], &arr[3]);
+        p.bytes.a1 = arr[0];
+        p.bytes.a2 = arr[1];
+        p.bytes.a3 = arr[2];
+        p.bytes.a4 = arr[3];
+        printf("%s->%u\n", str, p.num);
+    }
+    return 0;
+}
